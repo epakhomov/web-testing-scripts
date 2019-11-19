c4287801f06c6e3e44133de9912cd6ef9bd0b7b5
Eugene Pakhomov <e.i.pakhomov@gmail.com>
2019-11-18 22:29:18 -0800

    Create dotnet-core-sample.js

---
+++ dotnet-core-sample.js
@@ -0,0 +1,13 @@
+const puppeteer = require('puppeteer');
+  
+(async () => {
+  const browser = await puppeteer.launch({headless: true});
+  const page = await browser.newPage();
+  await page.goto('http://104.43.143.73:5000/Identity/Account/Login');
+  await page.type('#Input_Email', 'eugene.pakhomov@contrastsecurity.com')
+  await page.type('#Input_Password', '!Password123')
+  await page.click('button.btn.btn-primary')
+  await page.screenshot({path: 'example2.png'});
+
+  await browser.close();
+})();
